1735bf5b30fe0e7f3233d08bcf4e6bd05157aa60
F0002-211\bt-user <dic11249@gmail.com>
2022-04-11 10:58:06 +0800

    fix : 修正判斷縣市問題

--- app/Http/Services/WeatherService.php
+++ app/Http/Services/WeatherService.php
@@ -11,15 +11,15 @@ class WeatherService
 
     private $cityArr = [
         '臺北市', '新北市', '桃園市', '臺中市', '臺南市', '高雄市',
-        '新竹縣', '彰化縣', '南投縣', '雲林縣', '嘉義縣', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '澎湖縣' , '金門縣', '連江縣',
+        '新竹縣', '彰化縣', '南投縣', '雲林縣', '嘉義縣', '屏東縣', '宜蘭縣', '花蓮縣', '臺東縣', '澎湖縣', '金門縣', '連江縣',
         '基隆市', '新竹市', '嘉義市'
     ];
 
     public function getWeather(string $city, int $time = 0)
     {
-        $city = Str::replace('台','臺',$city);
+        $city = Str::replace('台', '臺', $city);
 
-        if (!Arr::exists($this->cityArr, $city)) {
+        if (!in_array($city, $this->cityArr)) {
             return $city;
         }
 
@@ -45,7 +45,7 @@ class WeatherService
                 'time' => $Wx[$i]['startTime'],
                 'Wx' => $Wx[$i]['parameter']['parameterName'],
                 'WxImg' => $Wx[$i]['parameter']['parameterValue'],
-                'url' => 'https://www.cwb.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/'.str_pad($Wx[$i]['parameter']['parameterValue'], 2,'0', STR_PAD_LEFT).'.svg',
+                'url' => 'https://www.cwb.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/' . str_pad($Wx[$i]['parameter']['parameterValue'], 2, '0', STR_PAD_LEFT) . '.svg',
                 'PoP' => $PoP[$i]['parameter']['parameterName'] . '%',
                 'Tem' => $MinT[$i]['parameter']['parameterName'] . '℃-' . $MaxT[$i]['parameter']['parameterName'] . '℃'
             ];
